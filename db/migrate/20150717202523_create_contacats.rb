0af809b653791cdd94348f6f48365c8e3f2f6383
James Nichols <jrnichols89@gmail.com>
2015-08-26 21:13:18 +0000

    fixed contact migration file

--- db/migrate/20150717202523_create_contacats.rb
+++ db/migrate/20150717202523_create_contacats.rb
@@ -1,9 +1,9 @@
-class CreateContacats < ActiveRecord::Migration
+class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string:name
       t.string:email
-      t.text:commets
+      t.text:comments
       
       t.timestamps
     end
