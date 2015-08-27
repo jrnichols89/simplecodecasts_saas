d254164b0d1927df6ab5745d5dccf40cdfcf68d3
James Nichols <jrnichols89@gmail.com>
2015-08-27 20:11:08 +0000

    fixed database file issue

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150818221002) do
   create_table "contacts", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.text     "commets"
+    t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
